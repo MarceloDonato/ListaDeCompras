07183d888629184554c78072e990f754ddda69c0
MarceloDonato <marcelo_donatoo@hotmail.com>
2020-06-24 19:53:44 -0300

    remove item

--- app/src/main/java/com/example/listadecompras/MainActivity.kt
+++ app/src/main/java/com/example/listadecompras/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.listadecompras
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +20,27 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    // para compartilhar a lista
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.share_menu){
+            val shareIntent = Intent().apply {
+                this.action = Intent.ACTION_SEND
+                this.putExtra(Intent.EXTRA_TEXT, "We are sharing data betwwen two app")
+                this.type = "text/plain"
+            }
+            startActivity(shareIntent)
+        }else{
+            return super.onOptionsItemSelected(item)
+        }
+        return true
+    }
+    // até aqui
+    // começo do codigo
     private fun setUpListeners() {
         add_item.setOnClickListener {
             val message = edittext_item.text.toString()
